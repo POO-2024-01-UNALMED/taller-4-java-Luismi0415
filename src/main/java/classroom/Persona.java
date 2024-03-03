d240c1be28f66c538e1f5b24acb011dd46a2aa30
Santiago Castro Herrera <159493032+Luismi0415@users.noreply.github.com>
2024-03-03 12:00:44 -0500

    1

--- src/main/java/classroom/Persona.java
+++ src/main/java/classroom/Persona.java
@@ -2,19 +2,18 @@ package classroom;
 
 public class Persona {
 
-    final long cedula;
+    long cedula;
     String nombre;
     static int totalPersonas;
     
     static {
-        totalPersonas = 0;
-        cedula = 3;
+    	totalPersonas=0;
     }
-
+   
     public Persona(long cedula, String nombre) {
         this.cedula = cedula;
         this.nombre = nombre;
-        totalPersonas++;
+        totalPersonas++ ;
     }
     
     public Persona(String nombre, long cedula) {
@@ -25,15 +24,15 @@ public class Persona {
 
     public Persona(long cedula) {
         this.cedula = cedula;
-        this.nombre = "";
         totalPersonas++;
     }
 
     public Persona(String nombre) {
-        this.nombre = "";
+        this.nombre = nombre;
         totalPersonas++;
     }
     
+       
     public long getCedula() {
         return cedula;
     }
